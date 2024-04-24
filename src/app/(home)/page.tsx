48afe1252978d60bb99f3df6b742bd78e42a02b4
JereUR <jeremias.jdv@gmail.com>
2024-04-23 22:07:38 -0300

    feat(Root): Homepage redirected and setted

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import BannerSection from '@/components/home/BannerSection'
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className="mt-12">
+      <BannerSection />
+    </div>
+  )
+}
