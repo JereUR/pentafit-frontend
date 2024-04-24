19ae6e466f124e5e0748e475cb6413b051b4408a
JereUR <jeremias.jdv@gmail.com>
2024-04-24 01:42:11 -0300

    feat(Root): Styles changed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import BannerSection from '@/components/home/BannerSection'
+import ServicesSection from '@/components/home/ServicesSection'
 import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className="mt-12">
+    <div>
       <BannerSection />
+      <ServicesSection />
     </div>
   )
 }
