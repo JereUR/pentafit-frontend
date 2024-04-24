da8dc63320a26166d216d12c97a04c2c0dfb2719
JereUR <jeremias.jdv@gmail.com>
2024-04-24 01:42:20 -0300

    feat(Root): Service section added

---
+++ src/components/home/ServicesSection.tsx
@@ -0,0 +1,84 @@
+import { IdCardIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+
+import servicesImg from '../../assets/services-test.png'
+
+const services = [
+  {
+    id: 1,
+    title: 'Service 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta saepe, odio laudantium explicabo facilis rerum quidem aperiam verominus libero quaerat delectus laborum esse nisi neque ratione rem. Quae!',
+    icon: <IdCardIcon className="w-8 h-8" />
+  },
+  {
+    id: 2,
+    title: 'Service 2',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta saepe, odio laudantium explicabo facilis rerum quidem aperiam verominus libero quaerat delectus laborum esse nisi neque ratione rem. Quae!',
+    icon: <IdCardIcon className="w-8 h-8" />
+  },
+  {
+    id: 3,
+    title: 'Service 3',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta saepe, odio laudantium explicabo facilis rerum quidem aperiam verominus libero quaerat delectus laborum esse nisi neque ratione rem. Quae!',
+    icon: <IdCardIcon className="w-8 h-8" />
+  },
+  {
+    id: 4,
+    title: 'Service 4',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta saepe, odio laudantium explicabo facilis rerum quidem aperiam verominus libero quaerat delectus laborum esse nisi neque ratione rem. Quae!',
+    icon: <IdCardIcon className="w-8 h-8" />
+  },
+  {
+    id: 5,
+    title: 'Service 5',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta saepe, odio laudantium explicabo facilis rerum quidem aperiam verominus libero quaerat delectus laborum esse nisi neque ratione rem. Quae!',
+    icon: <IdCardIcon className="w-8 h-8" />
+  }
+]
+
+export default function ServicesSection() {
+  return (
+    <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-5 items-center h-screen">
+      {services.map((s) => (
+        <div
+          key={s.id}
+          className={`w-full ${
+            s.id === 5 ? 'lg:col-span-2 lg:w-[40%]' : 'md:w-[80%]'
+          } min-h-[100px] p-8 mx-auto`}
+        >
+          <div className="my-6">
+            <div>
+              <p className="flex gap-3">
+                {s.icon}{' '}
+                <span className="text-2xl border-b border-white">
+                  {s.title}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <div className="ml-10 text-xl italic">{s.description}</div>
+            <div
+              className={`${
+                s.id === 5 ? 'h-[35vh] w-[35vw] ml-10' : 'h-[30vh] w-[30vw]'
+              }`}
+            >
+              <Image
+                src={servicesImg}
+                width={200}
+                height={200}
+                alt="Service photo"
+                className=" bg-gray-500 my-4 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
