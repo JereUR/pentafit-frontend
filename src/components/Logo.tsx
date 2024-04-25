85ece14b80f2e626d97829572079b3ffff47049b
JereUR <jeremias.jdv@gmail.com>
2024-04-25 02:31:19 -0300

    feat(Root): Margin x added

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Logo() {
   return (
-    <Link href={'/'} className="font-bold text-3xl hover:cursor-pointer">
+    <Link href={'/'} className="font-bold text-3xl hover:cursor-pointer mx-4">
       LogoGymApp
     </Link>
   )
