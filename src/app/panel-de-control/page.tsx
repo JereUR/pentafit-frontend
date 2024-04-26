c23a761e217a8c9e37493876bc22b2f42fb22f97
JereUR <jeremias.jdv@gmail.com>
2024-04-26 00:34:57 -0300

    fix(Root): Fix redirection

--- src/app/panel-de-control/page.tsx
+++ src/app/panel-de-control/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import useUser from '@/components/hooks/useUser'
+import { useRouter } from 'next/navigation'
 
 export default function DashboardPage() {
   const { user, token } = useUser()
+  const router = useRouter()
 
   if (!token) {
-    window.location.replace('/')
-    return null
+    router.push('/')
   }
 
   return <div>DashboardPage</div>
