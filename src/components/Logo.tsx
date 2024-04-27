5e5c21adf09ddc0f9fe0510ed52c66ea3d94f15e
JereUR <jeremias.jdv@gmail.com>
2024-04-26 21:56:51 -0300

    feat(Root): Colors managment

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,31 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
-export default function Logo() {
+export default function Logo({ isSticky }: { isSticky: boolean }) {
+  const pathname = usePathname()
+
+  let classname = ''
+
+  if (pathname != '/' && isSticky) {
+    classname = 'dark:text-muted'
+  } else {
+    classname = 'dark:text-white'
+  }
+
+  if (pathname === '/' && isSticky) {
+    classname = 'dark:text-white'
+  } else {
+    if (pathname === '/') classname = 'dark:text-muted'
+  }
+
   return (
-    <Link href={'/'} className="font-bold text-3xl hover:cursor-pointer mx-4">
-      LogoGymApp
+    <Link
+      href={'/'}
+      className={`font-bold text-4xl hover:cursor-pointer mx-8 ${classname}`}
+    >
+      PentaFit
     </Link>
   )
 }
