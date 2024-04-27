89eb771f3ebbe82340f5bededadd5757c20b13db
JereUR <jeremias.jdv@gmail.com>
2024-04-26 21:56:29 -0300

    feat(Root): Login form started

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -1,5 +1,15 @@
+import LoginForm from '@/components/LoginForm'
 import React from 'react'
 
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  return (
+    <div className="grid grid-cols-2 w-full mt-[12vh] h-[88vh]">
+      <div className="w-full h-full border border-foreground text-center">
+        <span>PHOTO</span>
+      </div>
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
