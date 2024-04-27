c88ae18b67916a399068d00870c3e8c678ebf969
JereUR <jeremias.jdv@gmail.com>
2024-04-27 04:37:34 -0300

    feat(Root): Image added

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -1,11 +1,17 @@
 import LoginForm from '@/components/LoginForm'
-import React from 'react'
+import photo from '../../assets/banner-login.png'
+import Image from 'next/image'
 
 export default function LoginPage() {
   return (
     <div className="grid grid-cols-2 w-full mt-[12vh] h-[88vh]">
-      <div className="w-full h-full border border-foreground text-center">
-        <span>PHOTO</span>
+      <div className="w-full m-4">
+        <Image
+          src={photo}
+          alt="login-banner"
+          objectFit="fill"
+          className="h-[80vh]"
+        />
       </div>
       <div>
         <LoginForm />
