a328d17620e1cf143c0a44564036892608ac61d3
JereUR <jeremias.jdv@gmail.com>
2024-04-29 00:27:35 -0300

    feat(Root): Register page added

---
+++ src/app/registrarse/page.tsx
@@ -0,0 +1,21 @@
+import photo from '../../../public/assets/banner-login.png'
+import Image from 'next/image'
+import RegisterForm from '@/components/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <div className="grid grid-cols-2 w-full mt-[12vh] h-[88vh]">
+      <div className="w-full m-4">
+        <Image
+          src={photo}
+          alt="login-banner"
+          objectFit="fill"
+          className="h-[80vh]"
+        />
+      </div>
+      <div>
+        <RegisterForm />
+      </div>
+    </div>
+  )
+}
