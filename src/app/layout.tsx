ef20b050560c1427282f894db168ffec849cf50a
JereUR <jeremias.jdv@gmail.com>
2024-04-29 22:22:22 -0300

    feat(Root): Managment for display or not layout depends of pathname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,40 @@
-import type { Metadata } from 'next'
+'use client'
+
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
 import AuthContextProvider from '@/components/context/AuthContext'
 import NavBar from '@/components/NavBar'
+import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'PentaFit'
-}
 
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const pathname = usePathname()
+  const shouldRenderLayout = !['/reestablecer'].includes(pathname)
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthContextProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <NavBar />
-            {children}
-          </ThemeProvider>
-        </AuthContextProvider>
+        {shouldRenderLayout ? (
+          <AuthContextProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <NavBar />
+              {children}
+            </ThemeProvider>
+          </AuthContextProvider>
+        ) : (
+          <>{children}</>
+        )}
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,11 @@
-'use client'
+import { Metadata } from 'next'
 
-import BannerSection from '@/components/home/BannerSection'
-import ContactForm from '@/components/home/ContactForm'
-import ServicesSection from '@/components/home/ServicesSection'
-import Logo from '@/components/Logo'
-import ThemeSwitcher from '@/components/ThemeSwitcher'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
+import HomePageComponent from '@/components/HomePageComponent'
+
+export const metadata: Metadata = {
+  title: 'PentaFit'
+}
 
 export default function HomePage() {
-  const servicesRef = useRef<HTMLDivElement | null>(null)
-
-  return (
-    <div className="flex flex-col min-w-full">
-      <main className="flex w-full justify-center pt-20 min-h-screen bg-primary-orange-400">
-        <div>
-          <BannerSection servicesRef={servicesRef} />
-        </div>
-      </main>
-      <div>
-        <ServicesSection servicesRef={servicesRef} />
-        <div className="flex justify-end">
-          <ContactForm />
-        </div>
-      </div>
-    </div>
-  )
+  return <HomePageComponent />
 }
