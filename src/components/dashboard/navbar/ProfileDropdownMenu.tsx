221d98c0f955d6b09ad0b8b2499c26b179706137
JereUR <jeremias.jdv@gmail.com>
2024-05-03 21:59:17 -0300

    feat(Root): User photo added in dropdown menu

--- src/components/dashboard/navbar/ProfileDropdownMenu.tsx
+++ src/components/dashboard/navbar/ProfileDropdownMenu.tsx
@@ -11,9 +11,11 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import noAvatarPhoto from '../../../../public/assets/noavatar.png'
+import profilePhoto from '../../../../public/assets/profile-photo.png'
 import useUser from '@/components/hooks/useUser'
 
 interface DropdownProp {
@@ -36,7 +38,7 @@ const dropdownMenuItems = [
 ]
 
 export default function ProfileDropdownMenu() {
-  const { logout } = useUser()
+  const { logout, user } = useUser()
 
   return (
     <div className="flex items-center justify-end">
@@ -45,7 +47,7 @@ export default function ProfileDropdownMenu() {
           <div className="cursor-pointer">
             <Image
               className="rounded-full object-cover p-[2px] ring-2 ring-primary-orange-600"
-              src={noAvatarPhoto}
+              src={profilePhoto}
               alt="Avatar photo"
               width={30}
               height={30}
@@ -53,20 +55,34 @@ export default function ProfileDropdownMenu() {
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="center" className="mt-1 p-2">
+          <DropdownMenuLabel className="flex flex-col justify-center items-center">
+            <div className="relative h-32">
+              <Image
+                src={profilePhoto}
+                alt="Tu imagen"
+                className="w-full h-full object-cover rounded-sm"
+              />
+              <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
+              <div className="absolute bottom-0 text-center p-1 w-full bg-background opacity-60 text-foreground text-xs z-20">
+                {user?.name}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator className="my-2" />
           {dropdownMenuItems.map((item: DropdownProp, key: number) => (
             <DropdownMenuItem
               key={key}
-              className="p-1 px-4 cursor-pointer rounded-r-full ml-[-10px]  transition flex items-start gap-2 hover:bg-primary-orange-500"
+              className="p-1 px-4 cursor-pointer rounded-r-full ml-[-10px]  transition flex items-start hover:bg-primary-orange-500"
             >
               <Link href={item.path}>
-                <span className="flex items-center gap-2">
+                <span className="flex items-center gap-2 my-1">
                   {item.icon}
                   {item.title}
                 </span>
               </Link>
             </DropdownMenuItem>
           ))}
-          <DropdownMenuItem className="p-1 px-4 cursor-pointer rounded-r-full ml-[-10px] transition flex items-start gap-2 hover:bg-primary-orange-500">
+          <DropdownMenuItem className="p-1 px-4 my-1 cursor-pointer rounded-r-full ml-[-10px] transition flex items-start gap-2 hover:bg-primary-orange-500">
             <span className="flex items-center gap-2 " onClick={logout}>
               <MdLogout />
               Cerrar Sesión
