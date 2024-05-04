4f4614601cb6f127ce47860b404766e55cfdfd1f
JereUR <jeremias.jdv@gmail.com>
2024-05-03 22:00:28 -0300

    feat(Root): Styles changes

--- src/components/dashboard/rightbar/RightBar.tsx
+++ src/components/dashboard/rightbar/RightBar.tsx
@@ -1,21 +1,13 @@
-import Image from 'next/image'
+import { Card } from '@/components/ui/card'
 import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md'
 
 const RightBar = () => {
   return (
     <div className="fixed mr-1">
-      <div className="relative bg-gradient-to-t from-gray-200 to-background dark:from-gray-700 py-5 px-6 rounded-lg mb-5">
-        <div className="absolute bottom-0 right-0 w-1/2 h-1/2">
-          <Image
-            className="object-contain opacity-20"
-            src="/astronaut.png"
-            alt="Astronaut"
-            fill
-          />
-        </div>
+      <Card className="relative bg-gradient-to-t from-bg-background to-bg-card py-5 px-6 rounded-lg mb-5 border-none shadow-md">
         <div className="flex flex-col gap-6">
           <span className="font-bold">🔥 Available Now</span>
-          <h3 className="font-medium text-xs text-gray-300">
+          <h3 className="font-medium text-xs text-foreground opacity-80">
             How to use the new version of the admin dashboard?
           </h3>
           <span className="subttitle">Takes 4 minutes to learn</span>
@@ -24,16 +16,16 @@ const RightBar = () => {
             illum laborum, alias obcaecati, dolorum iusto labore voluptatum
             quidem eos, aliquam maiores necessitatibus tempore rem quisquam.
           </p>
-          <button className="p-2 flex items-center gap-2 bg-purple-800 text-white border-none rounded-md cursor-pointer w-max">
+          <button className="p-2 flex items-center gap-2 bg-primary-orange-600 text-foregroundborder-none rounded-md cursor-pointer w-max">
             <MdPlayCircleFilled />
             Watch
           </button>
         </div>
-      </div>
-      <div className="relative bg-gradient-to-t from-gray-200 to-background dark:from-gray-700 py-5 px-6 rounded-lg mb-5">
+      </Card>
+      <Card className="relative bg-gradient-to-t from-bg-background to-bg-card py-5 px-6 rounded-lg mb-5 border-none shadow-md">
         <div className="flex flex-col gap-6">
           <span className="font-bold">🚀 Coming Soon</span>
-          <h3 className="font-medium text-xs text-gray-300">
+          <h3 className="font-medium text-xs text-foreground opacity-80">
             New server actions are available, partial pre-rendering is coming
             up!
           </h3>
@@ -43,12 +35,12 @@ const RightBar = () => {
             sunt corrupti similique, voluptatem, et quo maiores voluptates sed,
             ratione ipsam nobis. Ipsam voluptatem aliquid esse soluta ex?
           </p>
-          <button className="p-2 flex items-center gap-2 bg-purple-800 text-white border-none rounded-md cursor-pointer w-max">
+          <button className="p-2 flex items-center gap-2 bg-primary-orange-600 text-foreground border-none rounded-md cursor-pointer w-max">
             <MdReadMore size={20} />
             Learn
           </button>
         </div>
-      </div>
+      </Card>
     </div>
   )
 }
