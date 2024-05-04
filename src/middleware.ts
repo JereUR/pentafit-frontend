d038cb676eb41589b0d44e422c89c6251dec2b59
JereUR <jeremias.jdv@gmail.com>
2024-05-03 21:58:19 -0300

    feat(Root): Get session file name changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import getSession from './components/actions/getToken'
+import getSession from './components/actions/getSession'
 
 export async function middleware(request: NextRequest) {
   const { session } = await getSession()
