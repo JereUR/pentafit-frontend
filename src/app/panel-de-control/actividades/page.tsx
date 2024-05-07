21563a16d0475930f7ed416989de18a70d1bb938
JereUR <jeremias.jdv@gmail.com>
2024-05-07 03:16:06 -0300

    fix(Root): Sintaxis fixed

--- src/app/panel-de-control/actividades/page.tsx
+++ src/app/panel-de-control/actividades/page.tsx
@@ -1,5 +1,6 @@
-import ActivitiesTable from '@/components/dashboard/activities/ActiviesTable'
 import { Metadata } from 'next'
+
+import ActivitiesTable from '@/components/dashboard/activities/ActiviesTable'
 
 export const metadata: Metadata = {
   title: 'PentaFit - Actividades'
