c58204c87c6ec699060edef8902cc05a140e030b
JereUR <jeremias.jdv@gmail.com>
2024-05-08 02:11:51 -0300

    feat(Root): getSession function updated

--- src/components/actions/getSession.ts
+++ src/components/actions/getSession.ts
@@ -1,10 +1,51 @@
-export default async function getSession() {
+import axios from 'axios'
+import { NextRequest } from 'next/server'
+import { User } from '../types/User'
+import { cookies } from 'next/headers'
+
+/* export default async function getSession() {
   let session = null
   await fetch('https://jsonplaceholder.typicode.com/users/1')
     .then((response) => response.json())
     .then((json) => {
       session = json
     })
-
   return { session }
+} */
+
+export default async function getSession(
+  req: NextRequest
+): Promise<User | null> {
+  const sessionToken = cookies().get('session')
+  console.log(sessionToken)
+
+  if (!sessionToken) {
+    return null
+  }
+
+  return {
+    id: 2,
+    first_name: 'Jeremias',
+    last_name: 'DV',
+    email: 'jeremias.jdv@gmail.com',
+    photo_url: null,
+    token: sessionToken.toString()
+  }
+
+  try {
+    const response = await axios.post<User>(
+      '/api/validate-session',
+      { sessionToken },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return response.data
+  } catch (error) {
+    console.error('Error validating session:', error)
+    return null
+  }
 }
