049c6b4c8fa5a5d414d3bf6182f89f7d642be38d
JereUR <jeremias.jdv@gmail.com>
2024-05-10 02:02:27 -0300

    feat(Root): Recover password form added

--- src/components/session/RecoverForm.tsx
+++ src/components/session/RecoverForm.tsx
@@ -1,25 +1,70 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import useUser from '../hooks/useUser'
+import { useState } from 'react'
+import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons'
 
 export default function RecoverForm() {
+  const { updatePassword } = useUser()
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
-    <div className="bg-background w-[60vw] rounded-lg shadow-md p-10 flex flex-col gap-4">
-      <h2 className="text-3xl font-bold text-center mx-4 my-2 text-foreground">
-        Recuperar Contraseña
+    <div className="w-[60vw] shadow-lg p-8 flex flex-col gap-4">
+      <h2 className="text-4xl font-bold text-center m-4 text-foreground">
+        Reestablecer Contraseña
       </h2>
-      <span className="mx-4 my-2">
-        Ingrese si correo electrónico y recibirá un mail con la información
-        necesaria para reestablecer su contraseña.
-      </span>
-      <input
-        type="email"
-        placeholder="Correo electrónico"
-        className="bg-transparent border rounded-md text-xl p-2 focus:outline-none my-2 mx-4"
-      />
-      <Button
-        className="bg-primary-orange-600 h-[5vh] text-xl m-4 text-foreground rounded-md hover:bg-primary-orange-700"
-      >
-        Enviar
-      </Button>
+      <form action={updatePassword} className="py-6">
+        <div className="flex flex-col gap-4 mb-[2vh]">
+          <label htmlFor="password" className="text-lg font-extralight">
+            Contraseña
+          </label>
+          <div className="relative">
+            <input
+              type={`${showPassword ? 'text' : 'password'}`}
+              name="password"
+              className="bg-transparent border w-full rounded-md text-lg p-1 focus:outline-none"
+            />
+            <div
+              className="absolute inset-y-0 right-0 flex items-center mr-5 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <EyeOpenIcon className="h-5 w-5" />
+              ) : (
+                <EyeClosedIcon className="h-5 w-5" />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 mb-[2vh]">
+          <label htmlFor="confirm_password" className="text-lg font-extralight">
+            Confirmar Contraseña
+          </label>
+          <div className="relative">
+            <input
+              type={`${showPassword ? 'text' : 'password'}`}
+              name="confirm_password"
+              className="bg-transparent border w-full rounded-md text-lg p-1 focus:outline-none"
+            />
+            <div
+              className="absolute inset-y-0 right-0 flex items-center mr-5 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <EyeOpenIcon className="h-5 w-5" />
+              ) : (
+                <EyeClosedIcon className="h-5 w-5" />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <Button className="bg-primary-orange-600 h-[5vh] w-[40vw] text-xl m-4 text-foreground rounded-md transition duration-300 ease-in-out first-line:hover:bg-primary-orange-700 cursor-pointer">
+            Enviar
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
