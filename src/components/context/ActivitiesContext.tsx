952a56a84ddc58e0da4bff7e64c306f7d627b41b
JereUR <jeremias.jdv@gmail.com>
2024-05-14 03:54:38 -0300

    feat(Root): get activity by id and update activity actions added

--- src/components/context/ActivitiesContext.tsx
+++ src/components/context/ActivitiesContext.tsx
@@ -1,13 +1,6 @@
 'use client'
 
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useEffect,
-  useState
-} from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 import { Activity } from '../types/Activity'
 import axios from 'axios'
@@ -16,7 +9,9 @@ import { useToast } from '../ui/use-toast'
 type ActivitiesContextType = {
   activities: Activity[] | []
   getActivities: (q: string, page: string) => Promise<Activity[] | [] | void>
+  getActivityById: (id: string) => Promise<Activity | null>
   addActivity: (formData: FormData) => Promise<void | Error>
+  updateActivity: (formData: FormData) => Promise<void | Error>
 }
 
 export const ActivitiesContext = createContext<ActivitiesContextType | null>(
@@ -91,6 +86,7 @@ export default function ActivitiesContextProvider({
     }
   ])
   const { toast } = useToast()
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
   async function getActivities(
     q: string,
@@ -103,9 +99,7 @@ export default function ActivitiesContextProvider({
     params.append('regex', regex.toString())
     params.append('page', page)
     params.append('ITEMS_PER_PAGE', ITEM_PER_PAGE.toString())
-    const url = `${
-      process.env.BASE_BACKEND_URL
-    }get-activities?${params.toString()}`
+    const url = `${BASE_URL}get-activities?${params.toString()}`
 
     try {
       const response = await axios.get(url)
@@ -128,7 +122,104 @@ export default function ActivitiesContextProvider({
     }
   }
 
+  async function getActivityById(id: string) {
+    const url = `${BASE_URL}get-activity?id=${id}`
+    try {
+      const response = await axios.get(url)
+
+      if (response.status === 200 || response.status === 204) {
+        return response.data?.activity
+      } else {
+        toast({
+          title: 'Oh no! Algo salió mal.',
+          description: response.statusText
+        })
+      }
+    } catch (error: any) {
+      toast({
+        variant: 'destructive',
+        title: 'Oh no! Algo salió mal.',
+        description: error.message
+      })
+    }
+
+    return /* {
+      id: 'm5gr84i9',
+      activity: 'Actividad 1',
+      cost: 200,
+      isPublic: false,
+      quotaGeneration: true,
+      sessionMax: 5,
+      mpAvailable: false,
+      dateFrom: new Date(2024, 4, 20),
+      dateUntil: new Date(2024, 5, 20),
+      paymentType: 'Mensual'
+    } */null
+  }
+
   async function addActivity(formData: FormData): Promise<void | Error> {
+    const {
+      activity,
+      cost,
+      isPublic,
+      quotaGeneration,
+      sessionMax,
+      mpAvailable,
+      publicName,
+      dateFrom,
+      dateUntil,
+      paymentType
+    } = Object.fromEntries(formData)
+
+    let isPublicValue, quotaGenerationValue, mpAvailableValue
+
+    if (isPublic === undefined) {
+      isPublicValue = false
+    } else {
+      if (isPublic === 'true') {
+        isPublicValue = true
+      } else {
+        isPublicValue = false
+      }
+    }
+
+    if (quotaGeneration === undefined) {
+      quotaGenerationValue = false
+    } else {
+      if (quotaGeneration === 'true') {
+        quotaGenerationValue = true
+      } else {
+        quotaGenerationValue = false
+      }
+    }
+
+    if (mpAvailable === undefined) {
+      mpAvailableValue = false
+    } else {
+      if (mpAvailable === 'true') {
+        mpAvailableValue = true
+      } else {
+        mpAvailableValue = false
+      }
+    }
+
+    const newActivity = {
+      activity,
+      cost,
+      isPublic: isPublicValue,
+      quotaGeneration: quotaGenerationValue,
+      sessionMax,
+      mpAvailable: mpAvailableValue,
+      publicName,
+      dateFrom,
+      dateUntil,
+      paymentType
+    }
+
+    console.log(newActivity)
+  }
+
+  async function updateActivity(formData: FormData): Promise<void | Error> {
     const {
       activity,
       cost,
@@ -195,7 +286,9 @@ export default function ActivitiesContextProvider({
       value={{
         activities,
         getActivities,
-        addActivity
+        getActivityById,
+        addActivity,
+        updateActivity
       }}
     >
       {children}
