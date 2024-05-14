eace834d94ec7cf1cb1efb3602600e9e7b8f6b56
JereUR <jeremias.jdv@gmail.com>
2024-05-14 03:54:07 -0300

    feat(Root): Edit activity page added

---
+++ src/app/panel-de-control/actividades/editar/[id]/page.tsx
@@ -0,0 +1,9 @@
+import EditForm from '@/components/dashboard/activities/EditForm'
+
+export default function EditPage() {
+  return (
+    <div className="m-10">
+      <EditForm />
+    </div>
+  )
+}
