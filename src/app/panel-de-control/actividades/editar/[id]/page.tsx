9e48407bc29837c4b5d253d8d1e02a910d8c99ba
JereUR <jeremias.jdv@gmail.com>
2024-05-14 22:12:15 -0300

    feat(Root): Metadata incorporated

--- src/app/panel-de-control/actividades/editar/[id]/page.tsx
+++ src/app/panel-de-control/actividades/editar/[id]/page.tsx
@@ -1,43 +1,10 @@
-'use client'
+import EditActivity from '@/components/dashboard/activities/EditActivity'
+import { Metadata } from 'next'
 
-import ActivityForm from '@/components/dashboard/activities/ActivityForm'
-import useActivities from '@/components/hooks/useActivities'
-import { initialData, PropsAdd } from '@/components/types/Activity'
-import { usePathname, useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+export const metadata: Metadata = {
+  title: 'PentaFit - Editar actividad'
+}
 
 export default function EditPage() {
-  const pathname = usePathname()
-  const router = useRouter()
-  const [activity, setActivity] = useState<PropsAdd>(initialData)
-  const id = pathname.split('/')[4]
-  const { getActivityById, updateActivity } = useActivities()
-
-  useEffect(() => {
-    async function fetchActivity() {
-      const act = await getActivityById(id)
-      if (act) {
-        setActivity({
-          activity: act.activity,
-          cost: act.cost.toString(),
-          isPublic: act.isPublic ? 'true' : 'false',
-          quotaGeneration: act.quotaGeneration ? 'true' : 'false',
-          mpAvailable: act.mpAvailable ? 'true' : 'false',
-          publicName: act.publicName,
-          sessionMax: act.sessionMax.toString(),
-          dateFrom: act.dateFrom.toLocaleString(),
-          dateUntil: act.dateUntil.toLocaleString(),
-          paymentType: act.paymentType
-        })
-      }
-    }
-
-    fetchActivity()
-  }, [id])
-
-  return (
-    <div className="m-10">
-      <ActivityForm type="edit" activity={activity} />
-    </div>
-  )
+  return <EditActivity />
 }
---
+++ src/components/dashboard/activities/EditActivity.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import useActivities from '@/components/hooks/useActivities'
+import { initialData, PropsAdd } from '@/components/types/Activity'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import ActivityForm from './ActivityForm'
+
+export default function EditActivity() {
+  const pathname = usePathname()
+  const [activity, setActivity] = useState<PropsAdd>(initialData)
+  const id = pathname.split('/')[4]
+  const { getActivityById } = useActivities()
+
+  useEffect(() => {
+    async function fetchActivity() {
+      const act = await getActivityById(id)
+      if (act) {
+        setActivity({
+          id_companies: act.id_companies.map((c) => c.id),
+          activity: act.activity,
+          cost: act.cost.toString(),
+          isPublic: act.isPublic ? 'true' : 'false',
+          quotaGeneration: act.quotaGeneration ? 'true' : 'false',
+          mpAvailable: act.mpAvailable ? 'true' : 'false',
+          publicName: act.publicName,
+          sessionMax: act.sessionMax.toString(),
+          dateFrom: act.dateFrom.toLocaleString(),
+          dateUntil: act.dateUntil.toLocaleString(),
+          paymentType: act.paymentType
+        })
+      }
+    }
+
+    fetchActivity()
+  }, [id])
+
+  return (
+    <div className="m-10">
+      <ActivityForm type="edit" activity={activity} />
+    </div>
+  )
+}
