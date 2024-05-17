5d57054a056731f529838dd7ae67cdd509fd6264
JereUR <jeremias.jdv@gmail.com>
2024-05-17 02:53:05 -0300

    feat(Root): Button to add business added

--- src/components/dashboard/business/BusinessList.tsx
+++ src/components/dashboard/business/BusinessList.tsx
@@ -1,19 +1,17 @@
 'use client'
 
-import axios from 'axios'
 import BusinessItem from './BusinessItem'
-import { Business } from '@/components/types/Business'
-import { toast } from '@/components/ui/use-toast'
 import { useEffect, useState } from 'react'
 import useUser from '@/components/hooks/useUser'
 import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
 export default function BusinessList() {
   const [getData, setGetData] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const { businesses, getBusinesses } = useUser()
+  const router = useRouter()
+  const { loading, businesses, getBusinesses } = useUser()
 
   useEffect(() => {
     if (!getData) {
@@ -33,6 +31,14 @@ export default function BusinessList() {
       ) : (
         <div>No data</div>
       )}
+      <div className="flex justify-center">
+        <div
+          className="bg-transparent w-[5vw] h-[5vw] mb-6 text-7xl text-center text-foreground border-b border-lg border-foreground rounded-lg cursor-pointer transition duration-800 ease-in-out hover:text-green-600 hover:border-green-600 dark:hover:text-green-500 dark:hover:border-green-500  hover:animate-bounce"
+          onClick={() => router.push('/panel-de-control/negocios/agregar')}
+        >
+          +
+        </div>
+      </div>
     </div>
   )
 }
