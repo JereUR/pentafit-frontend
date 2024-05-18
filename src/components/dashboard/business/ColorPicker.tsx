fbf2c04e3bfc9f0901c9e5121b1ea9bbaeb5d888
JereUR <jeremias.jdv@gmail.com>
2024-05-17 22:38:32 -0300

    feat(Root): Color picker component for business form added

---
+++ src/components/dashboard/business/ColorPicker.tsx
@@ -0,0 +1,54 @@
+import {
+  useState,
+  useRef,
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useEffect
+} from 'react'
+import { PropsAddBusiness } from '@/components/types/Business'
+
+interface ColorPickerProps {
+  value: string
+  name: string
+  setDataBusiness: Dispatch<SetStateAction<PropsAddBusiness>>
+}
+
+const ColorPicker: React.FC<ColorPickerProps> = ({
+  value,
+  name,
+  setDataBusiness
+}) => {
+  const [color, setColor] = useState<string>(value)
+  const colorInputRef = useRef<HTMLInputElement>(null)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newColor = event.target.value
+    setColor(newColor)
+    setDataBusiness((prev) => ({ ...prev, [name]: newColor }))
+  }
+
+  useEffect(() => {
+    setColor(value)
+  }, [value])
+
+  return (
+    <div className="relative w-max p-2 flex justify-center items-center">
+      <input
+        type="color"
+        name={name}
+        className="w-12 h-12 opacity-0 absolute inset-0 cursor-pointer"
+        value={color}
+        onChange={handleChange}
+        ref={colorInputRef}
+      />
+      <div
+        className="w-8 h-8 rounded-full cursor-pointer ring-2 ring-foreground"
+        style={{ backgroundColor: color }}
+        onClick={() => colorInputRef.current?.click()}
+      />
+    </div>
+  )
+}
+
+export default ColorPicker
