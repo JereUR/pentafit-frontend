7b819f162ebd5e7eff5d03a6fcfe0b4e8ca6a90e
JereUR <jeremias.jdv@gmail.com>
2024-05-17 22:37:47 -0300

    feat(Root): Cache for session created

--- src/components/actions/getSession.ts
+++ src/components/actions/getSession.ts
@@ -4,17 +4,21 @@ import { Session } from '../types/User'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
+let sessionCache: {
+  [key: string]: { session: Session | null; timestamp: number }
+} = {}
+
 export default async function getSession(
   req: NextRequest
 ): Promise<Session | null> {
   const sessionToken = cookies().get('session')?.value
   let session: Session | null = null
 
-  if (!sessionToken) {
+  /* if (!sessionToken) {
     return null
-  }
+  } */
 
-  /*  session = {
+  session = {
     id: 2,
     first_name: 'Jeremias',
     last_name: 'DV',
@@ -23,7 +27,17 @@ export default async function getSession(
     token: 'sessionToken'
   }
 
-  return session */
+  return session
+
+  /* const CACHE_EXPIRATION_TIME = 5 * 60 * 1000
+  const currentTime = Date.now()
+
+  if (
+    sessionCache[sessionToken] &&
+    currentTime - sessionCache[sessionToken].timestamp < CACHE_EXPIRATION_TIME
+  ) {
+    return sessionCache[sessionToken].session
+  }
 
   const headers = {
     'X-Requested-With': 'XMLHttpRequest',
@@ -31,25 +45,22 @@ export default async function getSession(
   }
 
   try {
-    await fetch(`${BASE_URL}api/v1/currentuser`, {
+    const response = await fetch(`${BASE_URL}api/v1/currentuser`, {
       credentials: 'include',
       headers: headers
     })
-      .then((res) => res.json())
-      .then((data) => {
-        session = data
-      })
-      .catch((error) => {
-        console.error('Error validating session:', error)
-        return null
-      })
-      .finally(() => {
-        return session
-      })
+    const data = await response.json()
+    const session = data || null
+
+    // Actualiza el caché con la nueva sesión y el timestamp
+    sessionCache[sessionToken] = {
+      session: session,
+      timestamp: Date.now()
+    }
+
+    return session
   } catch (error) {
     console.error('Error validating session:', error)
     return null
-  }
-
-  return session
+  } */
 }
