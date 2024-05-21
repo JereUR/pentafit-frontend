d957ecf2417868537e896ceadb59ab4d78b2369e
JereUR <jeremias.jdv@gmail.com>
2024-05-20 22:20:51 -0300

    feat(Root): Type changes

--- src/components/types/Business.tsx
+++ src/components/types/Business.tsx
@@ -1,27 +1,54 @@
+export type Metadata = {
+  title?: string
+  primary_color?: string
+  secondary_color?: string
+  third_color?: string
+  slogan?: string
+}
+
 export type Business = {
   id: number
   name: string
   description?: string
+  email?: string
+  address?: string
+  phone?: string
+  instagram?: string
+  facebook?: string
   isActive: boolean
   isWorking: boolean
   logo?: string
-  metadata?: JSON
+  metadata?: Metadata
 }
 
 export interface PropsAddBusiness {
   name: string
   description?: string
+  email?: string
+  address?: string
+  phone?: string
+  instagram?: string
+  facebook?: string
   logo?: string
   title: string
   primary_color: string
   secondary_color: string
+  third_color: string
+  slogan?: string
 }
 
 export const initialData = {
   name: '',
   description: '',
+  email: '',
+  address: '',
+  phone: '',
+  instagram: '',
+  facebook: '',
   logo: '',
   title: '',
-  primary_color: '#FFFFFF',
-  secondary_color: '#FFFFFF'
+  primary_color: '#ec6409',
+  secondary_color: '#FFFFFF',
+  third_color: '#000000',
+  slogan: ''
 }
