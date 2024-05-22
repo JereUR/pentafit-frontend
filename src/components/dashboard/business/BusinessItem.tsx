d2196ee0746484093d824b853437b146683dbdec
JereUR <jeremias.jdv@gmail.com>
2024-05-21 22:29:13 -0300

    feat(Root): Styles changed

--- src/components/dashboard/business/BusinessItem.tsx
+++ src/components/dashboard/business/BusinessItem.tsx
@@ -15,7 +15,7 @@ import { Button } from '@/components/ui/button'
 export default function BusinessItem({ item }: { item: Business }) {
   return (
     <div className="p-4 m-8 border border-gray-300 dark:border-gray-600 rounded-lg">
-      <div className="flex py-6">
+      <div className="flex justify-between py-6">
         <div className={`flex ${!item.isActive && 'opacity-40'}`}>
           <div className="px-16">
             <Image
@@ -26,7 +26,7 @@ export default function BusinessItem({ item }: { item: Business }) {
               className="rounded-full ring-2 ring-primary-orange-600 dark:ring-primary-orange-400"
             />
           </div>
-          <div className="w-[50vw]">
+          <div className="">
             <h2 className="text-2xl font-bold">{item.name}</h2>
             <p className="text-lg">
               {item.description ? item.description : 'Sin descripción.'}
@@ -34,7 +34,7 @@ export default function BusinessItem({ item }: { item: Business }) {
             <p>{item.isActive ? 'Activo' : 'Inactivo'}</p>
           </div>
         </div>
-        <div className="flex flex-col justify-center gap-4 px-8 border-l">
+        <div className="flex flex-col gap-4 xl:px-8 border-l">
           <Button
             className={`flex justify-start items-center shadow-md dark:text-foreground gap-2 ${
               item.isWorking
