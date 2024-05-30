686bfea5b50f170db24e3114b8553ca6bada4cad
JereUR <jeremias.jdv@gmail.com>
2024-05-30 20:59:32 -0300

    fix: Errors fixed

--- src/components/dashboard/rightbar/RightBar.tsx
+++ src/components/dashboard/rightbar/RightBar.tsx
@@ -21,14 +21,12 @@ import { Business } from '@/components/types/Business'
 
 const RightBar = () => {
   const [showMenu, setShowMenu] = useState(false)
+  const [workingBusines, setWorkingBusines] = useState<
+    Business | undefined | null
+  >(null)
+  const [otherBusinesses, setOtherBusinesses] = useState<Business[] | []>([])
   const { token, businesses, getBusinesses, updateWorkingBusiness } = useUser()
   const router = useRouter()
-  const workingBusiness =
-    businesses.length > 0 ? businesses.filter((bus) => bus.is_working) : []
-  const otherBusinesses =
-    businesses.length > 0
-      ? businesses.filter((bus) => bus.is_active && !bus.is_working)
-      : []
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
   useEffect(() => {
@@ -36,6 +34,23 @@ const RightBar = () => {
       getBusinesses()
     }
   }, [token])
+
+  useEffect(() => {
+    if (businesses) {
+      if (businesses.length > 0) {
+        const newWorkingBusines = businesses.find((bus) => bus.is_working)
+        const newOthersBusinesses = businesses.filter(
+          (bus) => bus.is_active && !bus.is_working
+        )
+        if (newWorkingBusines) {
+          setWorkingBusines(newWorkingBusines)
+        }
+        if (newOthersBusinesses) {
+          setOtherBusinesses(newOthersBusinesses)
+        }
+      }
+    }
+  }, [businesses])
 
   async function handleChangeWorkingBusiness(id: number) {
     const res = await updateWorkingBusiness(id)
@@ -48,7 +63,7 @@ const RightBar = () => {
   return (
     <div className="fixed w-[250px] xl:w-[330px] m-4">
       <Card className="relative flex flex-col gap-6 bg-gradient-to-t from-bg-background to-bg-card p-4 rounded-lg shadow-md duration-300 ease-in-out">
-        {businesses.length > 0 ? (
+        {workingBusines ? (
           <>
             <p className="m-2 text-xl font-extralight">
               Area de trabajo actual:
@@ -56,8 +71,8 @@ const RightBar = () => {
             <div className="flex justify-center">
               <Image
                 src={
-                  workingBusiness[0].logo
-                    ? `${BASE_URL}${workingBusiness[0].logo}`
+                  workingBusines.logo
+                    ? `${BASE_URL}${workingBusines.logo}`
                     : noImage
                 }
                 alt="Card Image"
@@ -68,9 +83,11 @@ const RightBar = () => {
             </div>
             <div className="p-4">
               <h2 className="text-xl font-semibold">
-                {workingBusiness[0].name}
+                {workingBusines.name}
               </h2>
-              <p className="text-gray-600">{workingBusiness[0].description}</p>
+              <p className="text-gray-600">
+                {workingBusines.description}
+              </p>
               {otherBusinesses.length > 0 ? (
                 <DropdownMenu>
                   <DropdownMenuTrigger className="relative">
@@ -93,10 +110,13 @@ const RightBar = () => {
                       <DropdownMenuItem
                         key={key}
                         className="p-1 px-4 cursor-pointer rounded-r-full transition flex items-start hover:bg-primary-orange-500"
+                        onClick={() => handleChangeWorkingBusiness(item.id)}
                       >
                         <div className="flex justify-start items-center gap-2">
                           <Image
-                            src={item.logo ? `${BASE_URL}${item.logo}` : noImage}
+                            src={
+                              item.logo ? `${BASE_URL}${item.logo}` : noImage
+                            }
                             alt="Card Image"
                             width={35}
                             height={35}
