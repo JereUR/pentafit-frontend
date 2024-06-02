2cf2ee38c399cfce88950caa9990ce1354cc867d
JereUR <jeremias.jdv@gmail.com>
2024-06-02 01:32:25 -0300

    feat: Number of activities added in a card

--- src/components/dashboard/activities/ActiviesTable.tsx
+++ src/components/dashboard/activities/ActiviesTable.tsx
@@ -18,6 +18,7 @@ import { Business } from '@/components/types/Business'
 import SelectedActivitiesActions from './SelectedActivitiesActions'
 import noImage from '@public/assets/no-image.png'
 import Loader from '@/components/Loader'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
@@ -113,46 +114,58 @@ export default function ActivitiesTable() {
 
   return (
     <div className="container bg-background p-1 rounded-lg mt-5">
-      <div className="flex flex-col gap-6 mb-6 border border-gray-300 dark:border-gray-700 pt-2 pb-6 px-2">
-        <div className="flex gap-4 items-center">
-          <label className="text-xl font-light mt-4 ml-4">
-            Area de Trabajo
-          </label>
-        </div>
-        {workingBusiness ? (
-          <div className="flex items-center mx-6 mb-2">
-            <div className="flex px-6">
-              <Image
-                src={
-                  workingBusiness.logo
-                    ? `${BASE_URL}${workingBusiness.logo}`
-                    : noImage
-                }
-                alt={`${workingBusiness.name} logo`}
-                width={80}
-                height={80}
-                className="w-[80px] h-[80px] border-[1px] border-primary-orange-600 rounded-full p-1 dark:ring-primary-orange-400"
-              />
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-6 mb-6 border border-gray-300 rounded-md dark:border-gray-700 pt-2 pb-6 px-2">
+          <div className="flex gap-4 items-center">
+            <label className="text-xl font-light mt-4 ml-4">
+              Area de Trabajo
+            </label>
+          </div>
+          {workingBusiness ? (
+            <div className="flex items-center mx-6 mb-2">
+              <div className="flex px-6">
+                <Image
+                  src={
+                    workingBusiness.logo
+                      ? `${BASE_URL}${workingBusiness.logo}`
+                      : noImage
+                  }
+                  alt={`${workingBusiness.name} logo`}
+                  width={80}
+                  height={80}
+                  className="w-[80px] h-[80px] border-[1px] border-primary-orange-600 rounded-full p-1 dark:ring-primary-orange-400"
+                />
+              </div>
+              <div>
+                <h2 className="text-lg font-bold">{workingBusiness.name}</h2>
+                <p className="text-sm">
+                  {workingBusiness.description
+                    ? workingBusiness.description
+                    : 'Sin descripción.'}
+                </p>
+              </div>
             </div>
-            <div>
-              <h2 className="text-lg font-bold">{workingBusiness.name}</h2>
-              <p className="text-sm">
-                {workingBusiness.description
-                  ? workingBusiness.description
-                  : 'Sin descripción.'}
+          ) : (
+            <div className="flex flex-col justify-center items-center gap-2 ">
+              <p className="text-xl font-semibold">
+                Sin area de trabajo asignada
               </p>
+              <span className="text-sm italic">
+                Debes seleccionar un area de trabajo para realizar tareas
+              </span>
             </div>
-          </div>
-        ) : (
-          <div className="flex flex-col justify-center items-center gap-2 ">
-            <p className="text-xl font-semibold">
-              Sin area de trabajo asignada
-            </p>
-            <span className="text-sm italic">
-              Debes seleccionar un area de trabajo para realizar tareas
-            </span>
-          </div>
-        )}
+          )}
+        </div>
+        <div className='mr-10'>
+          <Card className="px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md">
+            <CardHeader className="text-5xl text-center py-1 text-primary-orange-500 font-semibold dark:text-primary-orange-700">
+              {activities.length}
+            </CardHeader>
+            <CardContent className="text-lg py-1 font-medium">
+              Actividades Totales
+            </CardContent>
+          </Card>
+        </div>
       </div>
       <div className="flex items-center justify-between my-4">
         <div className="flex justify-center gap-2">
