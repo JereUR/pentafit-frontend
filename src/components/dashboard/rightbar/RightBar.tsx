bfb27e1d36b33bf8aecfd6e4386fb479cc9d98be
JereUR <jeremias.jdv@gmail.com>
2024-06-07 20:18:42 -0300

    feat: Loader added to working business area

--- src/components/dashboard/rightbar/RightBar.tsx
+++ src/components/dashboard/rightbar/RightBar.tsx
@@ -23,7 +23,13 @@ const RightBar = () => {
     Business | undefined | null
   >(null)
   const [otherBusinesses, setOtherBusinesses] = useState<Business[] | []>([])
-  const { token, businesses, getBusinesses, updateWorkingBusiness } = useUser()
+  const {
+    token,
+    businesses,
+    getBusinesses,
+    updateWorkingBusiness,
+    loadingUser
+  } = useUser()
   const router = useRouter()
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL
 
@@ -59,72 +65,98 @@ const RightBar = () => {
   }
 
   return (
-    <div className="fixed w-[250px] xl:w-[330px] m-4">
+    <div className="fixed w-[250px] xl:w-[330px] m-5">
       <Card className="relative flex flex-col gap-6 bg-gradient-to-t from-bg-background to-bg-card p-4 rounded-lg border-none shadow-md duration-300 ease-in-out">
-        {workingBusines ? (
-          <>
-            <p className="m-2 text-xl font-extralight">
-              Area de trabajo actual:
-            </p>
-            <div className="flex justify-center">
-              <Image
-                src={
-                  workingBusines.logo
-                    ? `${BASE_URL}${workingBusines.logo}`
-                    : noImage
-                }
-                alt="Card Image"
-                width={150}
-                height={150}
-                className="w-[150px] h-[150px] border-2 border-primary-orange-600 rounded-full p-2 dark:ring-primary-orange-400"
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="text-xl font-semibold">{workingBusines.name}</h2>
-              <p className="text-gray-600">{workingBusines.description}</p>
-              {otherBusinesses.length > 0 ? (
-                <DropdownMenu>
-                  <DropdownMenuTrigger className="relative">
-                    <div className="xl:w-full flex justify-center items-center mt-4">
-                      <Button className="flex w-[180px] text-xs xl:w-full xl:text-base gap-2 items-center font-semibold transition duration-300 ease-in-out bg-primary-orange-700 hover:bg-primary-orange-600 cursor-pointer">
-                        Cambiar Area de Trabajo
-                        {showMenu ? (
-                          <MdExpandLess className="h-6 w-6" />
-                        ) : (
-                          <MdExpandMore className="h-6 w-6" />
-                        )}
+        {loadingUser ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {workingBusines ? (
+              <>
+                <p className="m-2 text-xl font-extralight">
+                  Area de trabajo actual:
+                </p>
+                <div className="flex justify-center">
+                  <Image
+                    src={
+                      workingBusines.logo
+                        ? `${BASE_URL}${workingBusines.logo}`
+                        : noImage
+                    }
+                    alt="Card Image"
+                    width={150}
+                    height={150}
+                    className="w-[150px] h-[150px] border-2 border-primary-orange-600 rounded-full p-2 dark:ring-primary-orange-400"
+                  />
+                </div>
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold">
+                    {workingBusines.name}
+                  </h2>
+                  <p className="text-gray-600">{workingBusines.description}</p>
+                  {otherBusinesses.length > 0 ? (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="relative">
+                        <div className="xl:w-full flex justify-center items-center mt-4">
+                          <Button className="flex w-[180px] text-xs xl:w-full xl:text-base gap-2 items-center font-semibold transition duration-300 ease-in-out bg-primary-orange-700 hover:bg-primary-orange-600 cursor-pointer">
+                            Cambiar Area de Trabajo
+                            {showMenu ? (
+                              <MdExpandLess className="h-6 w-6" />
+                            ) : (
+                              <MdExpandMore className="h-6 w-6" />
+                            )}
+                          </Button>
+                        </div>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        align="center"
+                        className="mt-5 w-[220px] xl:w-[280px] flex flex-col justify-center gap-2 p-3 bg-card shadow-md rounded-lg border border-primary-orange-600"
+                      >
+                        {otherBusinesses.map((item: Business, key: number) => (
+                          <DropdownMenuItem
+                            key={key}
+                            className="p-1 px-4 cursor-pointer rounded-r-full transition flex items-start hover:bg-primary-orange-500"
+                            onClick={() => handleChangeWorkingBusiness(item.id)}
+                          >
+                            <div className="flex justify-start items-center gap-2">
+                              <Image
+                                src={
+                                  item.logo
+                                    ? `${BASE_URL}${item.logo}`
+                                    : noImage
+                                }
+                                alt="Card Image"
+                                width={35}
+                                height={35}
+                                className="w-[35px] h-[35px] rounded-full"
+                              />
+                              <p className="text-sm font-semibold">
+                                {item.name}
+                              </p>
+                            </div>
+                          </DropdownMenuItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  ) : (
+                    <div className="flex flex-col justify-center w-full items-center mt-4">
+                      <span>No existen otros negocios activos</span>
+                      <Button
+                        className="flex gap-2 items-center font-semibold transition duration-300 ease-in-out bg-primary-orange-700 hover:bg-primary-orange-600"
+                        onClick={() =>
+                          router.push('/panel-de-control/negocios')
+                        }
+                      >
+                        Ir a sección Negocios
                       </Button>
                     </div>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent
-                    align="center"
-                    className="mt-5 w-[220px] xl:w-[280px] flex flex-col justify-center gap-2 p-3 bg-card shadow-md rounded-lg border border-primary-orange-600"
-                  >
-                    {otherBusinesses.map((item: Business, key: number) => (
-                      <DropdownMenuItem
-                        key={key}
-                        className="p-1 px-4 cursor-pointer rounded-r-full transition flex items-start hover:bg-primary-orange-500"
-                        onClick={() => handleChangeWorkingBusiness(item.id)}
-                      >
-                        <div className="flex justify-start items-center gap-2">
-                          <Image
-                            src={
-                              item.logo ? `${BASE_URL}${item.logo}` : noImage
-                            }
-                            alt="Card Image"
-                            width={35}
-                            height={35}
-                            className="w-[35px] h-[35px] rounded-full"
-                          />
-                          <p className="text-sm font-semibold">{item.name}</p>
-                        </div>
-                      </DropdownMenuItem>
-                    ))}
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              ) : (
+                  )}
+                </div>
+              </>
+            ) : (
+              <div>
+                <span>Area de Trabajo no asignada</span>
                 <div className="flex flex-col justify-center w-full items-center mt-4">
-                  <span>No existen otros negocios activos</span>
                   <Button
                     className="flex gap-2 items-center font-semibold transition duration-300 ease-in-out bg-primary-orange-700 hover:bg-primary-orange-600"
                     onClick={() => router.push('/panel-de-control/negocios')}
@@ -132,20 +164,8 @@ const RightBar = () => {
                     Ir a sección Negocios
                   </Button>
                 </div>
-              )}
-            </div>
-          </>
-        ) : (
-          <div>
-            <span>Area de Trabajo no asignada</span>
-            <div className="flex flex-col justify-center w-full items-center mt-4">
-              <Button
-                className="flex gap-2 items-center font-semibold transition duration-300 ease-in-out bg-primary-orange-700 hover:bg-primary-orange-600"
-                onClick={() => router.push('/panel-de-control/negocios')}
-              >
-                Ir a sección Negocios
-              </Button>
-            </div>
+              </div>
+            )}
           </div>
         )}
         {/* {showMenu && (
