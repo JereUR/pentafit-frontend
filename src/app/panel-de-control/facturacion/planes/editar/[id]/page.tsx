732c8e68b572799d6062d828ab6a9c311c095bf5
JereUR <jeremias.jdv@gmail.com>
2024-06-10 21:08:25 -0300

    feat: Edit plan page added

---
+++ src/app/panel-de-control/facturacion/planes/editar/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+import EditPlan from '@/components/dashboard/plans/EditPlan'
+
+export const metadata: Metadata = {
+  title: 'PentaFit - Editar plan'
+}
+
+export default function EditPage() {
+  return <EditPlan />
+}
