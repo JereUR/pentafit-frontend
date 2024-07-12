7fe658c03788694c8e006732a232b433b6aef2fa
JereUR <jeremias.jdv@gmail.com>
2024-07-12 01:14:37 -0300

    feat: Unnecesary imports and comments deleted. Select days to show component added

--- src/components/dashboard/diaries/DiariesTable.tsx
+++ src/components/dashboard/diaries/DiariesTable.tsx
@@ -3,36 +3,29 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
-import { FaEdit, FaTrash } from 'react-icons/fa'
 
-import Pagination from '../pagination/Pagination'
 import useDiaries from 'components/hooks/useDiaries'
 import CustomButton from 'components/CustomButton'
 import useUser from 'components/hooks/useUser'
 import { Business } from 'components/types/Business'
 import {
-  Columns,
-  daysOfWeek,
+  Days,
   Diary,
   DiaryGroup,
   GroupedData,
-  initialColumns
+  initialDays
 } from 'components/types/Diary'
 import WorkingBusiness from '../WorkingBusiness'
 import TableSkeleton from '../skeletons/TableSkeleton'
 import DiariesCalendar from './DiariesCalendar'
+import SelectDaysToShow from './SelectDaysToShow'
 
 export default function DiariesTable() {
   const [groupDiaries, setGroupDiaries] = useState<DiaryGroup[]>([])
   const [workingBusiness, setWorkingBusiness] = useState<Business | null>(null)
-  const [selectedDiaries, setSelectedDiaries] = useState<number[]>([])
-  const [selectAll, setSelectAll] = useState<boolean>(false)
   const [selectedItemsPerPage, setSelectedItemsPerPage] = useState<number>(5)
-  const [selectedColumns, setSelectedColumns] =
-    useState<Columns>(initialColumns)
-  const [showConfirmDeleteMap, setShowConfirmDeleteMap] = useState<{
-    [key: number]: boolean
-  }>({})
+  const [selectedDays, setSelectedDays] =
+    useState<Days>(initialDays)
 
   const searchParams = useSearchParams()
   const { token, getWorkingBusiness, loadingBusiness } = useUser()
@@ -64,8 +57,6 @@ export default function DiariesTable() {
     return groupedData
   }
 
-  console.log(groupDiaries)
-
   useEffect(() => {
     async function getData() {
       const res = await getWorkingBusiness()
@@ -93,10 +84,10 @@ export default function DiariesTable() {
 
   useEffect(() => {
     if (window != undefined) {
-      const columns = localStorage.getItem('columns-diaries')
+      const Days = localStorage.getItem('Days-diaries')
 
-      if (columns) {
-        setSelectedColumns(JSON.parse(columns))
+      if (Days) {
+        setSelectedDays(JSON.parse(Days))
       }
     }
   }, [])
@@ -107,103 +98,20 @@ export default function DiariesTable() {
     }
   }, [diaries])
 
-  const handleDelete = async (diary: number) => {
-    const diariesToDelete = [diary]
-
-    const res = await deleteDiariesById(diariesToDelete)
-
-    if (res) {
-      setShowConfirmDeleteMap((prevState) => ({
-        ...prevState,
-        [diary]: false
-      }))
-      window.location.reload()
-    }
-  }
-
-  const handleConfirmDelete = (diaryId: number) => {
-    setShowConfirmDeleteMap((prevState) => ({
-      ...prevState,
-      [diaryId]: true
-    }))
-  }
-
-  const handleCancelDelete = (diaryId: number) => {
-    setShowConfirmDeleteMap((prevState) => ({
-      ...prevState,
-      [diaryId]: false
-    }))
-  }
-
-  const handleSelectAllChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const checked = event.target.checked
-    setSelectAll(checked)
-
-    if (checked) {
-      setSelectedDiaries(diaries.map((diary) => diary.id))
-    } else {
-      setSelectedDiaries([])
-    }
-  }
-
-  const handleCheckboxChange = (
-    diaryId: number,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const checked = event.target.checked
-    const newSelectedDiaries = checked
-      ? [...selectedDiaries, diaryId]
-      : selectedDiaries.filter((id) => id !== diaryId)
-
-    setSelectedDiaries(newSelectedDiaries)
-  }
-
   return (
     <div className="container bg-background p-1 rounded-lg mt-10">
-      <div className="flex justify-between">
+      <div className="flex gap-10">
         <WorkingBusiness workingBusiness={workingBusiness} />
-        {/* {workingBusiness && loadingDiary ? (
-          <CountItemsSkeleton />
-        ) : (
-          <div className="mr-10">
-            <Card className="px-4 py-2 border border-none rounded-lg">
-              <CardHeader className="text-5xl text-center py-1 text-primary-orange-500 font-semibold dark:text-primary-orange-700">
-                {diaries.length}
-              </CardHeader>
-              <CardContent className="text-lg py-1 font-medium">
-                Agendas Totales
-              </CardContent>
-            </Card>
-          </div>
-        )} */}
       </div>
       <div className="flex items-center my-4">
-        {/* <div className="flex justify-center gap-2">
-          <Search placeholder="Buscar una agenda..." />
-          <SelectColumns
-            selectedColumns={selectedColumns}
-            setSelectedColumns={setSelectedColumns}
-          />
-          <SelectItemsPerPage
-            selectedItemsPerPage={selectedItemsPerPage}
-            setSelectedItemsPerPage={setSelectedItemsPerPage}
-          />
-          {workingBusiness && diaries.length > 0 && (
-            <ExportData business={workingBusiness} />
-          )}
-        </div> */}
         <div className="flex gap-4">
           <Link href="/panel-de-control/agenda/agregar">
             <CustomButton text="Agregar" />
           </Link>
-          {/* {selectedDiaries.length > 0 && (
-            <SelectedDiariesActions
-              selectedDiaries={selectedDiaries}
-              setSelectedDiaries={setSelectedDiaries}
-            />
-          )} */}
+          <SelectDaysToShow
+            selectedDays={selectedDays}
+            setSelectedDays={setSelectedDays}
+          />
         </div>
       </div>
 
@@ -211,7 +119,7 @@ export default function DiariesTable() {
         <TableSkeleton />
       ) : (
         <div>
-          <DiariesCalendar diaries={diaries} groupDiaries={groupDiaries} />
+          <DiariesCalendar diaries={diaries} groupDiaries={groupDiaries} selectedDays={selectedDays}/>
         </div>
       )}
     </div>
---
+++ src/components/dashboard/diaries/SelectDaysToShow.tsx
@@ -0,0 +1,89 @@
+import { Days, daysOfWeek } from '@/components/types/Diary'
+import { Button } from '@/components/ui/button'
+import { Dispatch, SetStateAction, useState } from 'react'
+import { FaTableCells } from 'react-icons/fa6'
+import { MdExpandLess, MdExpandMore } from 'react-icons/md'
+
+interface Props {
+  selectedDays: Days
+  setSelectedDays: Dispatch<SetStateAction<Days>>
+}
+
+const SelectDaysToShow: React.FC<Props> = ({
+  selectedDays,
+  setSelectedDays
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleChange = (name: number, value: boolean) => {
+    setSelectedDays({ ...selectedDays, [name]: value })
+  }
+
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleCloseMenu = () => {
+    setIsOpen(false)
+  }
+
+  const handleMenuClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation()
+  }
+
+  return (
+    <div className="relative">
+      <Button
+        variant="outline"
+        className="flex items-center gap-2 bg-card"
+        onClick={handleButtonClick}
+      >
+        <FaTableCells className="w-4 h-4" />
+        Días
+        {isOpen ? (
+          <MdExpandLess className="w-5 h-5" />
+        ) : (
+          <MdExpandMore className="w-5 h-5" />
+        )}
+      </Button>
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="absolute w-[20vw] xl:w-[15vw] bg-card mt-3 mr-5 rounded shadow-lg"
+        >
+          <div className="p-4" onClick={handleMenuClick}>
+            <p className="text-lg font-medium text-foreground">
+              Días a mostrar
+            </p>
+            <hr className="my-2 border-gray-200 dark:border-gray-500" />
+            {daysOfWeek.map((day, index) => (
+              <label key={day} className="flex items-center cursor-pointer ">
+                <input
+                  type="checkbox"
+                  className="mr-2 cursor-pointer"
+                  checked={selectedDays[index]}
+                  onChange={() => handleChange(index, !selectedDays[index])}
+                />
+                <span className="text-sm ml-2 mr-4 my-1 p-1 w-full rounded-r-full transition duration-500 ease-in-out hover:bg-primary-orange-600">
+                  {daysOfWeek[index]}
+                </span>
+              </label>
+            ))}
+
+            <hr className="my-2 border-gray-200 dark:border-gray-500" />
+            <div className="flex justify-center">
+              <Button
+                className="font-semibold text-foreground bg-background transition duration-300 ease-in-out hover:bg-accent w-full mx-4 mb-1 mt-2 p-1"
+                onClick={handleCloseMenu}
+              >
+                Confirmar
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SelectDaysToShow
