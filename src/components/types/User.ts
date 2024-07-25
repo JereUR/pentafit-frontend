5ed0876fcebd999a9088f59bb86439a5995388d8
JereUR <jeremias.jdv@gmail.com>
2024-07-24 21:09:04 -0300

    feat: Address property added

--- src/components/types/User.ts
+++ src/components/types/User.ts
@@ -14,6 +14,7 @@ export type User = {
   last_name: string
   phone: number
   photo?: string | null
+  address?: string
   role: string | null
 }
 
@@ -46,4 +47,54 @@ export interface PropsRegister {
 export interface PropsLogin {
   email: string
   password: string
+}
+
+export interface PropsUpdateProfile {
+  first_name: string
+  last_name: string
+  gender: string
+  birthdate: string
+  phone?: number
+  address?: string
+  actual_password?: string
+  new_password?: string
+  confirm_new_password?: string
+  [key: string]: string | undefined | number
+}
+
+export const initialDataUpdateProfile: PropsUpdateProfile = {
+  first_name: '',
+  last_name: '',
+  gender: '',
+  birthdate: '',
+  photo: undefined,
+  address: undefined,
+  actual_password: '',
+  new_password: '',
+  confirm_new_password: ''
+}
+
+export interface FormErrorsUpdateProfile {
+  first_name?: string
+  last_name?: string
+  gender?: string
+  birthdate?: string
+  photo?: string
+  address?: string
+  actual_password?: string
+  new_password?: string
+  confirm_new_password?: string
+  [key: string]: string | undefined
+}
+
+export const initialErrorsUpdateProfile: FormErrorsUpdateProfile = {
+  first_name: '',
+  last_name: '',
+  gender: '',
+  birthdate: '',
+  phone: undefined,
+  address: undefined,
+  actual_password: '',
+  new_password: '',
+  confirm_new_password: ''
 }
